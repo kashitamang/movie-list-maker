b3f6f7b5dad7138ff61866c2dc3e13d1a5f9b5a6
KASHI TAMANG <tamang.kashi@gmail.com>
2022-06-16 16:22:29 -0700

    adds some styling

--- src/Components/MovieForm.js
+++ src/Components/MovieForm.js
@@ -12,36 +12,35 @@ export default function MovieForm({
   setMovieColor,
 }) {
   return (
-    <div className="form-and-preview">
-      <div className="movie-form">
-        <form onSubmit={handleSubmit}>
-          <label>
+    <div className="movie-form">
+      <h2>Create New Movie</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
                 title
-            <input value={movieTitle} 
-              onChange={(e) => setMovieTitle(e.target.value)}/>
-          </label>
-          <label>
+          <input value={movieTitle} 
+            onChange={(e) => setMovieTitle(e.target.value)}/>
+        </label>
+        <label>
             year
-            <input value={movieYear}
-              onChange={(e) => setMovieYear(e.target.value)}/>
-          </label>
-          <label>
+          <input value={movieYear}
+            onChange={(e) => setMovieYear(e.target.value)}/>
+        </label>
+        <label>
             director
-            <input value={movieDirector}
-              onChange={(e) => setMovieDirector(e.target.value)}/>
-          </label>
-          <label>
+          <input value={movieDirector}
+            onChange={(e) => setMovieDirector(e.target.value)}/>
+        </label>
+        <label>
       color
-            <select value={movieColor} onChange={e => setMovieColor(e.target.value)}>
-              <option value="red">red</option>
-              <option value="blue">blue</option>
-              <option value="green">green</option>
-              <option value="yellow">yellow</option>
-            </select>
-          </label>
-          <button>add to watch list</button>
-        </form>
-      </div>    
-    </div>
+          <select value={movieColor} onChange={e => setMovieColor(e.target.value)}>
+            <option value="red">red</option>
+            <option value="blue">blue</option>
+            <option value="green">green</option>
+            <option value="yellow">yellow</option>
+          </select>
+        </label>
+        <button>add to watch list</button>
+      </form>
+    </div>    
   );
 }
